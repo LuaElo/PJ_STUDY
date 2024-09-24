2a3e80fff5f68921cdff3d323c24a2c7db8cf8be
eloahluara <eloahluaraa123@gmail.com>
2024-09-24 11:03:51 -0300

    atualização 2409 11:03

--- src/main/java/br/com/etechoracio/pw3_study/Pw3StudyApplication.java
+++ src/main/java/br/com/etechoracio/pw3_study/Pw3StudyApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pw3StudyApplication {
+public class
+
+Pw3StudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pw3StudyApplication.class, args);
--- src/main/java/br/com/etechoracio/pw3_study/controller/DisciplinaController.java
+++ src/main/java/br/com/etechoracio/pw3_study/controller/DisciplinaController.java
@@ -3,6 +3,7 @@ package br.com.etechoracio.pw3_study.controller;
 import br.com.etechoracio.pw3_study.dto.DisciplinaResponseDTO;
 import br.com.etechoracio.pw3_study.service.DisciplinaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/disciplinas")
-
+@CrossOrigin(origins = "*")
 public class DisciplinaController{
     @Autowired
     private DisciplinaService service;
